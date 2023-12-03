ea7052061aa360926c3b900ef3656882c1139eee
INFOPULSE\Tetiana.Dibrova <te.dibrova@gmail.com>
2023-12-04 00:09:39 +0200

    DibrovaSeleniumProject_HW14

---
+++ DibrovaSeleniumProject/UnitTest_HW14_Task1_ItemsInDropdown.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace DibrovaSeleniumProject
+{
+    public class UnitTest_HW14_Task1_ItemsInDropdown
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+
+        public void TearDown() 
+        { 
+            driver.Quit();
+        }
+
+        [Test]
+
+        public void VerifyNumberOfItemsInDropdown()
+        {
+            // Arrange
+            driver.Url = "https://www.globalsqa.com/demo-site/select-dropdown-menu/";
+            
+            IWebElement dropdown = driver.FindElement(By.XPath("//select"));
+        
+            // Act 
+            
+            string sizeAttribute = dropdown.GetAttribute("childElementCount");
+
+            int numberOfItems = int.Parse(sizeAttribute);
+
+            int expectedItems = 249;
+
+            // Assert
+
+            Assert.That(expectedItems == numberOfItems, $"Expected items '{expectedItems}' " +
+                $"isn't equal current items '{numberOfItems}'");
+        }
+    }
+}
---
+++ DibrovaSeleniumProject/UnitTest_HW14_Task2_OpenSelenium.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+namespace DibrovaSeleniumProject
+{
+    public class UnitTest_HW14_Task2_OpenSelenium
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+
+        public void VerifyOpeningSeleniumSite()
+        {
+            // Arrange
+            driver.Url = "https://www.google.com";
+
+            var expectedUrl = "https://www.selenium.dev/";
+
+            var actualURL = "https://www.selenium.dev/";
+
+            // Act 
+
+            IWebElement searchInput = driver.FindElement(By.XPath("//*[@jsname='yZiJbe']"));
+            searchInput.SendKeys("Selenium");
+
+            IWebElement searchButton = driver.FindElement(By.XPath("//*[@class='gNO89b'][@role='button']"));
+            searchButton.Click();
+
+            IWebElement seleniumLink = driver.FindElement(By.CssSelector("[class='byrV5b'] [class='tjvcx GvPZzd cHaqb']"));
+            seleniumLink.Click();
+
+            // Assert
+
+            Assert.That(expectedUrl == actualURL, $"Expected page URL '{expectedUrl}' " +
+                $"isn't equal actual page URL '{driver.Url}'");
+        }
+    }
+}
